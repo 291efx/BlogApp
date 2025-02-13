575e3f5ec492bc733250eca833e07f2c567c226d
erick <erick.fiestas.plaza.20@gmail.com>
2025-02-12 19:40:35 -0500

    Cambio de codigo

--- src/main/java/com/cibertec/blog/controller/HomeController.java
+++ src/main/java/com/cibertec/blog/controller/HomeController.java
@@ -1,15 +1,36 @@
 package com.cibertec.blog.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
-    @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("titulo", "Bienvenido a Mi Blog");
-        return "index"; // Retorna la vista index.html
+    @GetMapping
+    public String home() {
+        return "index"; // Renderiza index.html
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login"; // Renderiza login.html
+    }
+
+    @GetMapping("/publicacion/detalle")
+    public String publicacionDetalle() {
+        return "publicacion_detalle";
+    }
+
+    @GetMapping("/publicacion/nueva")
+    public String publicacionForm() {
+        return "publicacion_form";
+    }
+
+    @GetMapping("/registro")
+    public String registro() {
+        return "registro";
     }
 }
+
